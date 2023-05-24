95e6d1f6c05a9ae186afbd15d281604a12c13815
NForiben <foriben2006@icloud.com>
2023-05-23 23:03:39 -0400

    Added via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+import random
+
+player1 = input("What do you choose? Type 0 for Rock, 1 for paper or 2 for scissors.\n")
+
+if player1 == "0":
+  print(rock)
+elif player1 == "1":
+  print(paper)
+elif player1 == "2":
+  print(scissors)
+else:
+  print("Invalid Input")
+
+print("Computer chose:")
+
+list = [rock, paper, scissors]
+
+computer = random.randint(0,2)
+
+if computer == 0:
+  print(list[0])
+if computer == 1:
+  print(list[1])
+if computer == 2:
+  print(list[2])
+
+if player1 == "0" and computer == 2:
+  print("You win")
+elif player1 == "1" and computer == 0:
+  print("You win")
+elif player1 == "2" and computer == 1:
+  print("You win")
+else:
+  print("You lose")
